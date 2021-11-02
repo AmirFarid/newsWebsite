96dcd5079ecfa5d6b0dcad69ae9626ced39b96ac
Amir Farid <amirfarid1234@gmail.com>
2021-11-02 18:50:38 +0330

    refactor comment migrtation

--- database/migrations/2021_11_02_134437_create_comments_table.php
+++ database/migrations/2021_11_02_134437_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text("content");
             $table->unsignedBigInteger('post_id')->index();
             $table->unsignedBigInteger('user_id')->index();
 
